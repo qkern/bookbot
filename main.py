c299521d8a58549e98d1e57679d57ebbb4414f9f
qkern <qkernaghan@hotmail.com>
2024-01-15 19:16:24 -0500

    Finished program. Reads txt and prints report

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 def main():
     file_location = "books/frankenstein.txt"
-    book = copy_book(file_location)
-    words = word_count(book)
-    letters = letter_count(book)
+    text = copy_book(file_location)
+    words = word_count(text)
+    letters = letter_count(text)
     sorted_letters = letter_sort(letters)
 
     print(f"--- Begin report of {file_location} ---")
     print(f"There are {words} found in the document <br>")
+    print("")
+    print_letter_usage(sorted_letters)
     
 
     
 def copy_book(file_location):
     with open(file_location) as file:
-        file_contents = file.read()
-    return file_contents
+        text = file.read()
+    return text
 
-def word_count(file_contents):
-    return len(file_contents.split())
+def word_count(text):
+    return len(text.split())
 
-def letter_count(book):
+def letter_count(text):
     letters = {}
-    for char in book.lower():
+    for char in text.lower():
         if char in letters:
             letters[char] += 1
         else:
@@ -28,7 +30,12 @@ def letter_count(book):
     return letters
 
 def letter_sort(letters):
-    return 
+    letters_lst = list(letters.items())
+    return sorted(letters_lst, key= lambda letters_lst: letters_lst[1], reverse = True)
 
+def print_letter_usage(sorted_letters):
+    for letter in sorted_letters:
+        if letter[0].isalpha():
+            print(f"The '{letter[0]}' character was found {letter[1]} times")
 
 main()
